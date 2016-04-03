3c5eb4093ee8c7e356f7538b5e6840618ee7d029
lxb <chris0592@163.com>
2016-04-03 22:35:59 +0800

    分离datasource

---
+++ MoYu/Models/Requirement.swift
@@ -0,0 +1,18 @@
+//
+//  Copyright © 2015 Itty Bitty Apps. All rights reserved.
+
+import Foundation
+
+protocol Requirement {
+  var requiredClassName: String? { get }
+}
+
+extension Requirement {
+  var isAvailable: Bool {
+    guard let name = requiredClassName else {
+      return true
+    }
+
+    return NSClassFromString(name) != nil
+  }
+}
