24ef4374fca037e6e9d94aa92de47b170b98c4b0
lxb <chris0592@163.com>
2016-07-17 11:27:13 +0800

    add router

---
+++ MoYu/Miscellaneous/RemoteRequest/Router.swift
@@ -0,0 +1,17 @@
+//
+//  Router.swift
+//  MoYu
+//
+//  Created by Chris on 16/7/17.
+//  Copyright © 2016年 Chris. All rights reserved.
+//
+
+import Foundation
+
+enum Router {
+    case SignIn
+    case SignOut
+    case SignUp
+    
+    
+}
