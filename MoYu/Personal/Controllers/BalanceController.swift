79de5f9de9b66f7efa914720f26e968b35cfdc76
lxb <chris0592@163.com>
2016-07-26 17:21:54 +0800

    优化

--- MoYu/Personal/Controllers/BalanceController.swift
+++ MoYu/Personal/Controllers/BalanceController.swift
@@ -18,14 +18,31 @@ class BalanceController: BaseController {
         self.title = "财务信息"
     }
     
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self.navigationBarOpaque = false
+    }
+    
     
     //MARK: - private method
     private func setupView(){
         
+        self.view.backgroundColor = UIColor.whiteColor()
+        self.tableView.backgroundColor = UIColor.whiteColor()
+        
         self.view.addSubview( tableView )
         tableView.snp_makeConstraints { (make) in
-            make.edges.equalTo(self.view)
+            make.left.right.bottom.equalTo(self.view)
+            make.top.equalTo(self.view).offset(-64)
         }
+        
+        self.extendedLayoutIncludesOpaqueBars = true
+        
+        tableView.contentInset = UIEdgeInsets(top: headerDefaultHeight, left: 0, bottom: 0, right: 0)
+        
+        tableView.addSubview( self.balanceHeadView )
+        
     }
     
     //MARK: - var & let
@@ -48,9 +65,18 @@ class BalanceController: BaseController {
         return items
     }()
     
+    private let headerDefaultHeight:CGFloat = 280
+    private lazy var balanceHeadView: BalanceHeadView = {
+        let view = BalanceHeadView(frame:CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.headerDefaultHeight))
+        return view
+    }()
 }
 
 extension BalanceController : UITableViewDelegate{
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 0.01
+    }
     
     func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
         
@@ -75,3 +101,31 @@ extension BalanceController : UITableViewDataSource{
         return cell
     }
 }
+
+
+//MARK: - UIScrollView
+extension BalanceController{
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        
+        let y = scrollView.contentOffset.y + 64
+        if y < -headerDefaultHeight {
+            var frame = balanceHeadView.frame
+            frame.origin.y = y
+            frame.size.height = -y
+            balanceHeadView.frame = frame
+        }
+        
+        //切换透明背景
+        if y - 2 > -headerDefaultHeight {
+            if !self.navigationBarOpaque{
+                self.navigationBarOpaque = true
+            }
+        } else {
+            if self.navigationBarOpaque{
+                self.navigationBarOpaque = false
+            }
+        }
+    }
+}
+
--- MoYu/Personal/Views/BalanceHeadView.swift
+++ MoYu/Personal/Views/BalanceHeadView.swift
@@ -13,6 +13,8 @@ class BalanceHeadView: UIView {
   
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        layout()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -20,12 +22,149 @@ class BalanceHeadView: UIView {
     }
     
     
+    @objc private func withdrawButtonClicked(button:UIButton){
+        println("提现")
+    }
+    
     private func layout(){
     
+        //top
+        self.addSubview(cycleView)
+        cycleView.snp_makeConstraints { (make) in
+            make.center.equalTo(self )
+            make.size.equalTo(CGSize(width: 100, height: 100))
+        }
+        
+        cycleView.addSubview(balanceLabel)
+        balanceLabel.snp_makeConstraints { (make) in
+            make.center.equalTo(self.cycleView)
+        }
+        
+        cycleView.addSubview(balanceTitleLabel)
+        balanceTitleLabel.snp_makeConstraints { (make) in
+            make.centerX.equalTo(balanceLabel)
+            make.bottom.equalTo(balanceLabel.snp_top)
+        }
+        
+        //bottom
+        self.addSubview(consumeDetailLabel)
+        consumeDetailLabel.snp_makeConstraints { (make) in
+            make.centerX.equalTo(self)
+            make.bottom.equalTo(self).offset(-10)
+        }
+        
+        self.addSubview(leftLineView)
+        leftLineView.snp_makeConstraints { (make) in
+            make.left.equalTo(self).offset(16)
+            make.centerY.equalTo(consumeDetailLabel)
+            make.right.equalTo(consumeDetailLabel.snp_left).offset(-10)
+            make.height.equalTo(0.5)
+        }
+        
+        self.addSubview(rightLineView)
+        rightLineView.snp_makeConstraints { (make) in
+            make.right.equalTo(self).offset(-16)
+            make.centerY.equalTo(consumeDetailLabel)
+            make.left.equalTo(consumeDetailLabel.snp_right).offset(10)
+            make.height.equalTo(leftLineView)
+        }
+        
+        //middle
+        self.addSubview(withdrawView)
+        withdrawView.snp_makeConstraints { (make) in
+            make.centerX.equalTo(self)
+            make.bottom.equalTo(consumeDetailLabel).offset(-30)
+            make.top.greaterThanOrEqualTo(cycleView).offset(20)
+        }
+        
+        withdrawView.addSubview(withdrawImageView)
+        withdrawImageView.snp_makeConstraints { (make) in
+            make.left.top.bottom.equalTo(withdrawView)
+            make.size.equalTo(CGSize(width: 20, height: 20))
+        }
+        
+        withdrawView.addSubview(withdrawLabel)
+        withdrawLabel.snp_makeConstraints { (make) in
+            make.right.top.bottom.equalTo(withdrawView)
+            make.height.equalTo(withdrawImageView)
+            make.left.equalTo(withdrawImageView.snp_right).offset(5)
+        }
     }
     
     //MARK: - var & let
+    let cycleView:UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.mo_lightYellow()
+        view.layer.cornerRadius = 50
+        view.layer.masksToBounds = true
+        return view
+    }()
+    
+    let balanceLabel:UILabel = {
+        let label = UILabel()
+        label.font = UIFont.mo_boldFont(.biggest)
+        label.textColor = UIColor.mo_silver()
+        label.textAlignment = .Center
+        label.text = "8800.00"
+        return label
+        
+    }()
+    
+    let balanceTitleLabel:UILabel = {
+        let label = UILabel()
+        label.font = UIFont.mo_font(.smaller)
+        label.textColor = UIColor.mo_silver()
+        label.textAlignment = .Center
+        label.text = "余额(元)"
+        return label
+    }()
+    
+    let withdrawView : UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.whiteColor()
+        view.layer.borderColor = UIColor.mo_lightBlack().CGColor
+        view.layer.borderWidth = 0.8
+        return view
+    }()
+    
+    private let withdrawImageView:UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "defalutHead")
+        return imageView
+    }()
+    
+    private let withdrawLabel:UILabel = {
+        let label = UILabel()
+        label.text = "提现 "
+        label.textColor = UIColor.mo_lightBlack()
+        label.font = UIFont.mo_font()
+        return label
+    }()
+    
+    private lazy var withdrawButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("", forState: .Normal)
+        button.addTarget(self, action: #selector(BalanceHeadView.withdrawButtonClicked(_:)), forControlEvents: .TouchUpInside)
+        return button
+    }()
     
     
+    private let leftLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.mo_silver()
+        return view
+    }()
+    private let rightLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.mo_silver()
+        return view
+    }()
     
+    private let consumeDetailLabel:UILabel = {
+        let label = UILabel()
+        label.text = "余额明细"
+        label.textColor = UIColor.mo_silver()
+        label.font = UIFont.mo_font(.smaller)
+        return label
+    }()
 }
