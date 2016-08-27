83db8719782798945d400742178ef03fac20feb5
lxb <lianxb@xmnode.com>
2016-08-27 23:14:49 +0800

    debug

--- MoYu/MoKit/DatePicker/DatePickerController.swift
+++ MoYu/MoKit/DatePicker/DatePickerController.swift
@@ -16,12 +16,12 @@ class DatePickerController: UIViewController {
         super.viewDidLoad()
         
         self.setupView()
-        
-        self.view.backgroundColor = UIColor ( red: 0.0, green: 0.0, blue: 0.0, alpha: 0.3 )
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
+        
+        self.view.backgroundColor = UIColor ( red: 0.0, green: 0.0, blue: 0.0, alpha: 0.5 )
         
         contentView.animation = "slideUp"
         contentView.animate()
